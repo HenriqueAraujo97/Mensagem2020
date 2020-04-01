2650f51a9910a1d3d02f0b4b79e4dcb3cc062c56
HenriqueAraujo97 <araujo.henrique311@gmail.com>
2020-04-01 10:38:00 +0100

    Criação de uma Viewn no button App Mensagem2020

--- app/src/main/java/pt/ipg/mensagem2020/MainActivity.java
+++ app/src/main/java/pt/ipg/mensagem2020/MainActivity.java
@@ -2,7 +2,12 @@ package pt.ipg.mensagem2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +16,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+  
 }
