d223aa7c4957b0f0c822eee8a4f2f32c6f2449a2
HenriqueAraujo97 <araujo.henrique311@gmail.com>
2020-04-01 10:54:57 +0100

    Visualização da mensagem ao carregar no botao

--- app/src/main/java/pt/ipg/mensagem2020/MainActivity.java
+++ app/src/main/java/pt/ipg/mensagem2020/MainActivity.java
@@ -10,11 +10,19 @@ import android.widget.EditText;
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-  
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editTextMensagemText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
 }
